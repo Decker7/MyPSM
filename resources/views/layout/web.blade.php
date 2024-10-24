bb819e3fcdfdf0e5e9b52605caaadda1b34f295c
Decker7 <afiqm6526@gmail.com>
2024-10-24 22:56:25 +0800

    first commit

---
+++ resources/views/layout/web.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+
+    {{-- top navbar  --}}
+
+    @include('layout.navigation')
+
+    {{-- Page content goes here --}}
+    <div class="bg-white">
+        @yield('content')
+    </div>
+    
+    {{-- footer --}}
+
+    @include('layout.footer')
+
+    
+
+</body>
+
+</html>
